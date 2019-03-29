19a49d39f67db7f3bc726b03750a77da85d27550
frublox <lainislaw@gmail.com>
2019-03-29 11:48:44 -0400

    Fix bounds on Bellman Ford

--- src/Sssp.hs
+++ src/Sssp.hs
@@ -53,7 +53,7 @@ dijkstra graph source = go unvisitedInit distInit HashMap.empty
                 prev' = insertAll prev (fmap snd updates)
 
 bellmanFord :: Graph -> Node -> (HashMap Node Distance, HashMap Node Node)
-bellmanFord graph source = go 0 distInit HashMap.empty
+bellmanFord graph source = go 1 distInit HashMap.empty
     where
         distInit :: HashMap Node Distance
         distInit = HashMap.fromList (nodeDistPairsInit graph source)
@@ -63,7 +63,7 @@ bellmanFord graph source = go 0 distInit HashMap.empty
             -> HashMap Node Node
             -> (HashMap Node Distance, HashMap Node Node)
         go i dist prev =
-            if i < fromIntegral (Graph.numNodes graph)
+            if i <= fromIntegral (Graph.numNodes graph) - 1
                 then go (i + 1) dist' prev'
                 else (dist, prev)
             where
@@ -84,8 +84,8 @@ nodeDistPairsInit graph source =
         nonSourceNodes = filter (/= source) (Graph.nodeList graph)
 
 -- Tries to improve the distance estimate for the endpoint of the given edge
-relax :: HashMap Node Distance 
-    -> (Edge, Weight) 
+relax :: HashMap Node Distance
+    -> (Edge, Weight)
     -> Maybe ((Node, Distance), (Node, Node))
 relax dist (edge, weight) =
     if newEst < vDist
